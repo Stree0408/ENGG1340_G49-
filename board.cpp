1d7092bce658a19a3939ea450b8763b3cdeffb25
u3605051 <149300138+u3605051@users.noreply.github.com>
2023-11-07 00:26:56 +0800

    Update board.cpp
    Created struct Board with:
    int columns, int rows (maybe will be deleted if the number of columns and rows are static?)
    int xLocation, int yLocation: for the current location of the user's 'cursor'.
    char playerBoard[16][30]: for the board. Not finalized yet.
    
    Created the basic outline by adding the following functions:
    void setBoard()
    void printBoard()
    void getPlayerInput()
    void uncover()
    void flagging()

--- board.cpp
+++ board.cpp
@@ -1,6 +1,97 @@
 #include "board.h"
 #include <iostream>
+using namespace std;
 
-void board() {
 
-}
+struct Board {
+    const int columns;
+    const int rows;
+    int xLocation;
+    int yLocation;
+    char playerBoard[16][30];
+
+    void setBoard() {
+
+    }
+
+    void printBoard() {
+        system("clear");
+    }
+
+    void getPlayerInput() {
+        char playerInput;
+        printBoard();
+        cout << "input the direction you want to head. "
+                "if you put in multiple characters, "
+                "only the first one will be considered.";
+
+        while (true) {
+            cin >> playerInput;
+
+            switch (playerInput) {
+            case 'w':
+                if (yLocation + 1 >= rows) {
+                    ++yLocation;
+                    return;
+                } else {
+                    printBoard();
+                    cout << "invalid input. please go to another direction.";
+                    continue;
+                }
+
+            case 'a':
+                if (xLocation - 1 < 0) {
+                    --xLocation;
+                    return;
+                } else {
+                    printBoard();
+                    cout << "invalid input. please go to another direction.";
+                    continue;
+                }
+
+            case 's':
+                if (yLocation - 1 < 0) {
+                    --yLocation;
+                    return;
+                } else {
+                    printBoard();
+                    cout << "invalid input. please go to another direction.";
+                    continue;
+                }
+
+            case 'd':
+                if (xLocation + 1 >= columns) {
+                    ++xLocation;
+                    return;
+                } else {
+                    printBoard();
+                    cout << "invalid input. please go to another direction.";
+                    continue;
+                }
+
+            case 'o':
+                uncover();
+                return;
+
+            case 'p':
+                flagging();
+                return;
+
+            default:
+                cout << "invalid input";
+                continue;
+        }
+        }
+
+    }
+
+    void uncover() {
+
+    }
+
+    void flagging() {
+
+    }
+
+
+};
