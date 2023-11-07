a9e267478174b3d0fb4b13684ec8ce1e497df1c2
u3605051 <149300138+u3605051@users.noreply.github.com>
2023-11-08 07:32:04 +0800

    Made the code more readable
    No big changes.

--- board.cpp
+++ board.cpp
@@ -1,4 +1,4 @@
-#include "board.h"
+#include "runGame.h"
 #include <iostream>
 using namespace std;
 
@@ -10,88 +10,98 @@ struct Board {
     int yLocation;
     char playerBoard[16][30];
 
-    void setBoard() {
-
-    }
-
-    void printBoard() {
-        system("clear");
-    }
-
-    void getPlayerInput() {
-        char playerInput;
-        printBoard();
-        cout << "input the direction you want to head. "
-                "if you put in multiple characters, "
-                "only the first one will be considered.";
-
-        while (true) {
-            cin >> playerInput;
-
-            switch (playerInput) {
-            case 'w':
-                if (yLocation + 1 >= rows) {
-                    ++yLocation;
-                    return;
-                } else {
-                    printBoard();
-                    cout << "invalid input. please go to another direction.";
-                    continue;
-                }
-
-            case 'a':
-                if (xLocation - 1 < 0) {
-                    --xLocation;
-                    return;
-                } else {
-                    printBoard();
-                    cout << "invalid input. please go to another direction.";
-                    continue;
-                }
-
-            case 's':
-                if (yLocation - 1 < 0) {
-                    --yLocation;
-                    return;
-                } else {
-                    printBoard();
-                    cout << "invalid input. please go to another direction.";
-                    continue;
-                }
-
-            case 'd':
-                if (xLocation + 1 >= columns) {
-                    ++xLocation;
-                    return;
-                } else {
-                    printBoard();
-                    cout << "invalid input. please go to another direction.";
-                    continue;
-                }
-
-            case 'o':
-                uncover();
-                return;
-
-            case 'p':
-                flagging();
-                return;
-
-            default:
-                cout << "invalid input";
-                continue;
-        }
-        }
-
-    }
-
-    void uncover() {
-
-    }
-
-    void flagging() {
-
-    }
-
+    void setBoard();
+    void printBoard();
+    void getPlayerInput();
+    void uncover();
+    void flagging();
 
 };
+
+
+void Board::setBoard() {
+}
+
+
+void Board::printBoard() {
+    system("clear");
+}
+
+
+void Board::getPlayerInput() {
+    char playerInput;
+    printBoard();
+    cout << "input the direction you want to head. "
+            "if you put in multiple characters, "
+            "only the first one will be considered.";
+
+    bool validInput = false;
+    do {
+        cin >> playerInput;
+
+        switch (playerInput) {
+        case 'w':
+            if (yLocation + 1 >= rows) {
+                ++yLocation;
+                validInput = true;
+            }
+            break;
+
+        case 'a':
+            if (xLocation - 1 < 0) {
+                --xLocation;
+                validInput = true;
+            }
+            break;
+
+        case 's':
+            if (yLocation - 1 < 0) {
+                --yLocation;
+                validInput = true;
+            }
+            break;
+
+        case 'd':
+            if (xLocation + 1 >= columns) {
+                ++xLocation;
+                validInput = true;
+            }
+            break;
+
+        case 'o':
+            uncover();
+            validInput = true;
+            break;
+
+        case 'p':
+            flagging();
+            validInput = true;
+            break;
+
+        default:
+            cout << "invalid input";
+            break;
+        }
+        
+        if (!validInput) {
+            printBoard();
+            cout << "invalid input";
+        }
+
+    } while (!validInput);
+
+}
+
+
+void Board::uncover() {
+}
+
+
+void Board::flagging() {
+}
+
+
+
+void runGame(Board b){
+    b.setBoard();
+}
