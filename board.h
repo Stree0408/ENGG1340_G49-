e1c82ab3b24b2c57aa0c1d6645af44f8f5984ff4
Stree0408 <146194500+Stree0408@users.noreply.github.com>
2023-11-23 23:50:55 +0800

    Update board.h

--- board.h
+++ board.h
@@ -1,13 +1,31 @@
-#ifndef main_dynamic_h
-#define main_dynamic_h
+#ifndef execute_h
+#define execute_h
 
-void customize();
-void make_board();
-void print_board(); 
-void print_tmp_board();
-int cnt_mines(int x, int y);
-void place_mines(int x, int y);
-void del_dynamic();
+// 항상 row * col (not col * row)
+struct Board {
+    int row;
+    int col;
+    int nMines;
+    int x; // x coordinate of the player's cursor
+    int y; // y coordinate of the player's cursor
+    char **mainBoard; // board with only 'B'
+    char **playerBoard; // board that the player sees
+        // these char arrays need to be edited so that their columns and rows are changeable.   
+
+    void customize();
+    void setBoard();    // set mainBoard and playerBoard
+    void printMainBoard();
+    void printPlayerBoard();
+    int cntMines(int x, int y);
+    void placeMines(int x, int y);
+    void delDynamic();
+    
+    
+//    int currentScore;
+//    char getPlayerInput(bool IsFirstTimePlaying);
+//    void uncover(int x, int y);
+//    void flagging();
+};
 
 
-#endif /* main_dynamic_h */
+#endif /* execute_h */
