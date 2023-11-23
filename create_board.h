57a5a80460e1f02d9be9ab3a66ba55c0ec3ffeba
Stree0408 <146194500+Stree0408@users.noreply.github.com>
2023-11-23 17:14:48 +0800

    Create create_board.h

---
+++ create_board.h
@@ -0,0 +1,13 @@
+#ifndef main_dynamic_h
+#define main_dynamic_h
+
+void customize();
+void make_board();
+void print_board(); 
+void print_tmp_board();
+int cnt_mines(int x, int y);
+void place_mines(int x, int y);
+void del_dynamic();
+
+
+#endif /* main_dynamic_h */
