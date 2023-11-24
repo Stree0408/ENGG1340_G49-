970320cf36657f6877c5c7814e0a83b59f8a087c
angella127 <149568071+angella127@users.noreply.github.com>
2023-11-25 01:16:17 +0800

    score.cpp

--- score.cpp
+++ score.cpp
@@ -9,25 +9,44 @@ struct Score {
     int score;
 };
 
-void saveScore(const Score& score) {
-    ofstream file("scoreboard.txt", ios::app);
+void saveScoreboard(const vector<Score>& scoreboard) {
+    ofstream file("scoreboard.txt");
     if (file.is_open()) {
-        file << score.playerName << " " << score.score << endl;
+        for (const Score& score : scoreboard) {
+            file << score.playerName << " " << score.score << endl;
+        }
         file.close();
     } else {
         cout << "Unable to open the scoreboard file." << endl;
     }
 }
 
-void updateScoreboard(const string& playerName, int points) {
+void updateScoreboard(const string& playerName, int points, vector<Score>& scoreboard) {
     Score score;
     score.playerName = playerName;
     score.score = points;
     
-    saveScore(score);
+    scoreboard.push_back(score);
+    saveScoreboard(scoreboard);
+}
+
+void loadScoreboard(vector<Score>& scoreboard) {
+    ifstream file("scoreboard.txt");
+    if (file.is_open()) {
+        Score score;
+        while (file >> score.playerName >> score.score) {
+            scoreboard.push_back(score);
+        }
+        file.close();
+    } else {
+        cout << "Unable to open the scoreboard file." << endl;
+    }
 }
 
 int main() {
+    vector<Score> scoreboard;
+    loadScoreboard(scoreboard);
+
     string playerName;
     int points;
     
@@ -37,7 +56,23 @@ int main() {
     cout << "Enter points: ";
     cin >> points;
     
-    updateScoreboard(playerName, points);
+    updateScoreboard(playerName, points, scoreboard);
+
+    // Ask the user whether to continue from the previous game or start new
+    cout << "Do you want to continue from the previous game? (y/n): ";
+    char choice;
+    cin >> choice;
+
+    if (choice == 'y' || choice == 'Y') {
+        // Continue from the previous game
+        for (const Score& score : scoreboard) {
+            cout << "Player: " << score.playerName << ", Score: " << score.score << endl;
+        }
+    } else {
+        // Start a new game
+        scoreboard.clear();
+        saveScoreboard(scoreboard);
+    }
     
     return 0;
 }
