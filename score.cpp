a21d4f745ee4d2d2ca7b342e7b56c1e31ff48147
angella127 <149568071+angella127@users.noreply.github.com>
2023-11-26 01:00:08 +0800

    Update score.cpp

--- score.cpp
+++ score.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <vector>
+#include <algorithm>
 #include "score.h"
 
 using namespace std;
 
+void printScoreboard(const vector<Score>& scoreboard) {
+    cout << "Scoreboard:" << endl;
+    for (const Score& score : scoreboard) {
+        cout << "Player: " << score.playerName << ", Score: " << score.record << endl;
+    }
+}
+
+// Comparator function to sort scores in descending order
+bool compareScores(const Score& score1, const Score& score2) {
+    return score1.record > score2.record;
+}
+
+// Function to sort the scores from highest to lowest
+void sortScores(vector<Score>& scoreboard) {
+    sort(scoreboard.begin(), scoreboard.end(), compareScores);
+}
 
 
 void loadScoreboard(vector<Score>& scoreboard);
