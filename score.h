561a32615c0c031444e109969b80b51ebdfc4c56
angella127 <149568071+angella127@users.noreply.github.com>
2023-11-26 00:45:54 +0800

    Update score.h

--- score.h
+++ score.h
@@ -1,7 +1,17 @@
 #ifndef score_H
 #define score_H
 
- void saveScore();
- void updateScoreboard();
+#include <string>
+#include <vector>
+
+struct Score {
+    std::string playerName;
+    int record;
+};
+
+void saveScoreboard(const std::vector<Score>& scoreboard);
+void updateScoreboard(const std::string& playerName, int points);
+void loadScoreboard(std::vector<Score>& scoreboard);
 
 #endif
+
