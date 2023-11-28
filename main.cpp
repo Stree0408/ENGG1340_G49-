878b3dc8b528c63316857bc8651b4b8b280372a0
u3605051 <149300138+u3605051@users.noreply.github.com>
2023-11-29 07:16:00 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 using namespace std;
 
+
+//run the main game
 void runGame() {
     Board b;
     b.setBoardSize();
     b.setBoard();
     b.xLocation = 3;
     b.yLocation = 3;
-    b.currentScore = 5000;
+    b.currentScore = 3000;
     b.numberOfFlags = 0;
     char input;
     bool determine; 
@@ -29,9 +31,12 @@ void runGame() {
         determine = b.checkEndGame();
     } while (input != 'm' && determine==false);
 
-    // needs a function to end the game and save the scores and stuff.
-    cout << "GAME OVER!!!" << endl;
+    //ending displays
+    system("clear");
+    displayEnding(b.currentScore);
     b.endGame();
+
+    //deletes memory
     b.delDynamic();
 }
 
@@ -39,18 +44,30 @@ void runGame() {
 int main() {
     displayBanner();
     mainMenu();
+
     int firstinput;
     cin >> firstinput;
+
     while (firstinput != 1 && firstinput != 2) {
         cout << "Please enter the number again: ";
         cin >> firstinput;
     }
+
     if(firstinput == 1){
-        runGame();} 
-    // need function for leaderboard
-    else if (firstinput ==2) {
-        leaderboard();
         runGame();
+    } else if (firstinput == 2) {
+        system("clear");
+        displayBanner();
+        leaderboard();
+        cout << "\n\nPlease input 's' if you want to start the game. Else, the program will end.\n\n"
+                "Input: ";
+
+        char input;
+        cin >> input;
+        if (input == 's' || input == 'S') {
+            runGame();
+        }
     }
+
     return 0;
 }
